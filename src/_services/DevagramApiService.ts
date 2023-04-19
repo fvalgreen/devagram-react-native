0eff7513a4c6ade1b4625d7f5e3f22fe493dd73c
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-04-19 20:19:35 -0300

    Conexão com api

---
+++ src/_services/DevagramApiService.ts
@@ -0,0 +1,41 @@
+import axios, {Method} from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+
+const URL = 'https://devagram-node-nextjs-wheat.vercel.app/api';
+
+const instance = axios.create({
+  baseURL: URL,
+  timeout: 3000
+});
+
+export const api = async (endpoint: string, method: Method, body?: any, newHeader?: any) => {
+  const token = await AsyncStorage.getItem('token');
+
+  let headers: any = newHeader? newHeader : {'Content-type' : 'application/json'};
+
+  if(token){
+    headers['Authorization'] = 'Bearer ' + token;
+  };
+
+  console.log(`executando: ${URL}${endpoint}, metodo ${method}, body ${JSON.stringify(body)}, headers ${JSON.stringify(headers)}`);
+
+  return instance.request({
+    url: endpoint,
+    method: method,
+    data: body? body : '',
+    headers: headers,
+  });
+};
+
+const post = async (url: string, data: any, headers?: any) => {
+  re;turn api(url, "POST", data, headers);
+}
+const get = async (url: string, headers?: any) => {
+  return api(url, "GET", headers);
+};
+const put = async (url: string, data: any, headers?: any) => {
+  return api(url, "PUT", data, headers);
+};
+
+export {put, get, post};
